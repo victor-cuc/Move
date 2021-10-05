94a4c0db79ac88953e43e6d8e40142777b49d08c
Victor Cuc <victorcuc99@yahoo.com>
2021-10-05 10:15:14 +0300

    Initial Commit

---
+++ Move/MoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveApp.swift
+//  Move
+//
+//  Created by Victor Cuc on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
