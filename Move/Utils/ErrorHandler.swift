fa83c746ed73a5fc0fa1d3a6be585fcd426810da
Victor Cuc <victorcuc99@yahoo.com>
2021-10-13 16:27:47 +0300

    display error message as SwiftMessage for auth

--- Move/Utils/ErrorHandler.swift
+++ Move/Utils/ErrorHandler.swift
@@ -13,8 +13,34 @@ class ErrorHandler {
         switch error {
         case let apiError as APIError:
             print(apiError.message)
+            showErrorMessage(message: apiError.message)
         default:
             print(error.localizedDescription)
         }
+    }
+    
+    static func showErrorMessage(message: String) {
+        let view = MessageView.viewFromNib(layout: .cardView)
+
+        // Theme message elements with the warning style.
+        view.configureTheme(.error)
+
+        // Add a drop shadow.
+        view.configureDropShadow()
+
+        // Set message title, body, and icon. Here, we're overriding the default warning
+        // image with an emoji character.
+        view.configureContent(title: "Error", body: message)
+        view.button?.isHidden = true
+        
+        // Increase the external margin around the card. In general, the effect of this setting
+        // depends on how the given layout is constrained to the layout margins.
+        view.layoutMarginAdditions = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+
+        // Reduce the corner radius (applicable to layouts featuring rounded corners).
+        (view.backgroundView as? CornerRoundingView)?.cornerRadius = 10
+
+        // Show the message.
+        SwiftMessages.show(view: view)
     }
 }
